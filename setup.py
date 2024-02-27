339d5207f2e6a6afcb79f5d03ef4db16384a62f7
Konrad Wolsing <wolsing@comsys.rwth-aachen.de>
2024-02-27 15:39:24 +0100

    Version 1.3.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ipal-transcriber",
-    version="1.3.6",
+    version="1.3.7",
     packages=find_packages(exclude="tests"),
     scripts=[
         "ipal-transcriber",
